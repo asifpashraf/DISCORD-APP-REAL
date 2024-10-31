118cb207d15a2df795cf7576f16468dacc7071ec
unknown <asifpashraf@gmail.com>
2024-10-31 12:57:51 +0530

    ci

--- src/Discover-component/Discover.jsx
+++ src/Discover-component/Discover.jsx
@@ -20,7 +20,11 @@ function Discover() {
     {title:"dargonaballz",online:'234542',offline:243576,picture:Dream},
     {title:"arcade",online:"123432",offline:243576,picture:fairy},
     {title:"messiah",online:'465755',offline:243576,picture:FantasyMain},
-    {title:"Dojo",online:'465755',offline:243576,picture:Hydro}
+    {title:"Dojo",online:'465755',offline:243576,picture:Hydro},
+    {title:"dargonaballz",online:'234542',offline:243576,picture:Dream},
+    {title:"arcade",online:"123432",offline:243576,picture:fairy},
+    {title:"messiah",online:'465755',offline:243576,picture:FantasyMain},
+    {title:"Dojo",online:'465755',offline:243576,picture:Hydro},
   ]
   
   
